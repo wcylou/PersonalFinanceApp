87e675fb009d64811197b16c5291d9e960b1ac0c
Alex Murphy <alexander.f.murphy@gmail.com>
2018-07-30 16:38:47 -0600

    angular bits

--- ngPersonalFinance/src/app/models/income-stream.ts
+++ ngPersonalFinance/src/app/models/income-stream.ts
@@ -1,2 +1,13 @@
 export class IncomeStream {
+  id: number;
+  expectedAmount: number;
+  userId: number;
+  categoryId: number;
+  amount: number;
+
+  constructor(
+    id?: number
+  ) {
+    this.id = id;
+  }
 }
