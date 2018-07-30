34c4d9ee4d7ac830d117e78184cacebf81290d35
Alex Murphy <alexander.f.murphy@gmail.com>
2018-07-30 16:52:06 -0600

    models on angular

--- ngPersonalFinance/src/app/models/income-stream.ts
+++ ngPersonalFinance/src/app/models/income-stream.ts
@@ -1,13 +1,24 @@
 export class IncomeStream {
   id: number;
-  expectedAmount: number;
   userId: number;
   categoryId: number;
-  amount: number;
+  expectedAmount: number;
+  startDate: Date;
+  recurrencesPerYear: number;
 
   constructor(
-    id?: number
+    id?: number,
+    userId?: number,
+    categoryId?: number,
+    expectedAmount?: number,
+    startDate?: Date,
+    recurrencesPerYear?: number
   ) {
     this.id = id;
+    this.userId = userId;
+    this.categoryId = categoryId;
+    this.expectedAmount = expectedAmount;
+    this.startDate = startDate;
+    this.recurrencesPerYear = recurrencesPerYear;
   }
 }
--- ngPersonalFinance/src/app/models/user.ts
+++ ngPersonalFinance/src/app/models/user.ts
@@ -1,2 +1,24 @@
 export class User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+  active: number;
+
+  constructor(
+    id?: number,
+    username?: string,
+    password?: string,
+    email?: string,
+    role?: string,
+    active?: number
+  ) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+    this.active = active;
+  }
 }
