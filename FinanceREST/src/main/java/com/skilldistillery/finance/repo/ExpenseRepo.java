44f3094bd4e5eaa7154aae07b6f392cd96c810d4
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-07-31 15:39:59 -0600

    merge

--- FinanceREST/src/main/java/com/skilldistillery/finance/repo/ExpenseRepo.java
+++ FinanceREST/src/main/java/com/skilldistillery/finance/repo/ExpenseRepo.java
@@ -4,11 +4,15 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.finance.entities.Expense;
 
 public interface ExpenseRepo extends JpaRepository<Expense, Integer>{
-	List<Expense> findByUser_UsernameAndDateBetween(String username, Date start, Date end);
+	
+	@Query("Select e from Expense e where (e.date between :start and :end) AND e.user.username = :username")
+	List<Expense> findByUser_UsernameAndDateBetween(@Param("start") Date start,@Param("end") Date end, @Param("username") String username);
 	
 	Expense findByUser_UsernameAndId(String username, int id);
 	List<Expense> findByUser_Username(String username);
