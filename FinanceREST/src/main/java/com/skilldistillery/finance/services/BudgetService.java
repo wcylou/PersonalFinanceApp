4fe8c57e5eb8ede771e650fd79176111d9755eb4
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-07-31 13:24:15 -0600

    added java crud for budget

--- FinanceREST/src/main/java/com/skilldistillery/finance/services/BudgetService.java
+++ FinanceREST/src/main/java/com/skilldistillery/finance/services/BudgetService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.finance.services;
 
+import java.util.List;
+
+import com.skilldistillery.finance.entities.Budget;
+
 public interface BudgetService {
+
+	boolean destroy(String username, int id);
+
+	Budget update(String username, int id, Budget budget);
+
+	Budget create(String username, Budget budget);
+
+	Budget show(String username, int id);
+
+	List<Budget> indexBudget(String username);
 
 }
--- FinanceREST/src/main/java/com/skilldistillery/finance/services/BudgetServiceImpl.java
+++ FinanceREST/src/main/java/com/skilldistillery/finance/services/BudgetServiceImpl.java
@@ -1,8 +1,56 @@
 package com.skilldistillery.finance.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.finance.entities.Budget;
+import com.skilldistillery.finance.entities.Expense;
+import com.skilldistillery.finance.repo.BudgetRepo;
+import com.skilldistillery.finance.repo.UserRepo;
 
 @Service
 public class BudgetServiceImpl implements BudgetService{
-
+	@Autowired
+	BudgetRepo budRepo;
+	@Autowired
+	UserRepo userRepo;
+	
+	@Override
+	public List<Budget> indexBudget(String username) {
+		return budRepo.findByUser_Username(username);
+	}
+	
+	@Override
+	public Budget show(String username, int id) {
+		return budRepo.findByUser_UsernameAndId(username, id);
+	}
+	
+	@Override
+	public Budget create(String username, Budget budget) {
+		budget.setUser(userRepo.findByUsername(username));
+		return budRepo.saveAndFlush(budget);
+	}
+	
+	@Override
+	public Budget update(String username, int id, Budget budget) {
+		Budget e = show(username, id);
+		e.setAmount(budget.getAmount());
+		e.setStartDate(budget.getStartDate());
+		e.setEndDate(budget.getEndDate());
+		e.setDescription(budget.getDescription());
+		e.setExpenseCategory(budget.getExpenseCategory());
+		return budRepo.saveAndFlush(e);
+	}
+	
+	@Override
+	public boolean destroy(String username, int id) {
+		try {
+			budRepo.delete(show(username, id));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
